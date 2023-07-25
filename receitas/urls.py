ff7f8dcaf8693155572702c89317ed22edbfdfad
Alison Matos <133253035+alisonmaloo@users.noreply.github.com>
2023-07-25 12:27:00 -0300

    adicionando funçoes

---
+++ receitas/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path 
+from receitas.views import home, contato, sobre
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', home),
+    path('contato/', contato),
+    path('sobre/', sobre),
+
+]
