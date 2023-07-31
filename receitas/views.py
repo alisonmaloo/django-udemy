1c94a920e244b956784d0befb0fb311919b34b23
Alison Matos <133253035+alisonmaloo@users.noreply.github.com>
2023-07-31 12:38:13 -0300

    montagem de head da pagina

--- receitas/views.py
+++ receitas/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'receitas/pages/home.html', context={name: 'Alison Matos'})
+    return render(request, 'receitas/pages/home.html', context={'name':'da Dona Regina', })
 
 
 # Create your views here.
