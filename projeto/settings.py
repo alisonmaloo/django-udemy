63e6d563e9f17de1960db8777fa63dcba6789983
Alison Matos <133253035+alisonmaloo@users.noreply.github.com>
2023-08-02 13:41:11 -0300

    alteraçoes

--- projeto/settings.py
+++ projeto/settings.py
@@ -93,9 +93,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'base_static',
 ]
-STATIC_ROOT= BASE_DIR/ 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
-
-
 def home(request):
-    return render(request, 'recipes/pages/home.html', context={'name': 'da Dona Regina', })
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Receitas da Regi',
+    })
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/home.html', context={'name': 'Receitas da Regi',
+    })
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'name': 'Receitas da Regi',
+    })
