0a9a4d66c99cbdc8c7c45b6175efd942bfb071c5
Alison Matos <133253035+alisonmaloo@users.noreply.github.com>
2023-08-03 16:55:42 -0300

    alteraçoes

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,11 @@
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Receitas da Regi',
+        'recipes': [make_recipe() for _ in range(10)],
     })
-
-
 def recipe(request, id):
-    return render(request, 'recipes/pages/home.html', context={'name': 'Receitas da Regi',
-    })
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'name': 'Receitas da Regi',
+        'recipe': make_recipe(),
+        'is_detail_page': True,
     })
